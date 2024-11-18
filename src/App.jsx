6e3db13d76d1cf3e07e398a2d97e49e4eec185f7
Isura-udith <120595046+Isura-udith@users.noreply.github.com>
2024-11-18 12:24:41 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl font-bold text-blue-600 pt-14'>AgriNet project</h1>
+    </>
+  )
+}
+
+export default App
